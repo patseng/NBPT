2702991114b497633405fcc2915177dd3a2604ff
Peter Tseng <patseng@stanford.edu>
2011-11-23 12:08:05 -0800

    Initial Commit

---
+++ NBPT/NBPTAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  NBPTAppDelegate.h
+//  NBPT
+//
+//  Created by Peter Tseng on 11/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NBPTAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
